76617b3b578ffc72db56534f88205ac3a31b7a3c
CODER <122457095+IamJhonsnow@users.noreply.github.com>
2023-05-26 14:47:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jhonbrave/url-auto-delete-shortener-bot
+  git clone https://github.com/IamJhonsnow/withouturl
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
